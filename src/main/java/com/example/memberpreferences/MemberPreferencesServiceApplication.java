bee6babb3611f2a35bdc9910957c8ed392b531cf
Rutuja Jadhav <rutuja.jadhav@Rutujas-Mac-mini.local>
2026-07-22 14:59:49 +0530

    First commit

---
+++ src/main/java/com/example/memberpreferences/MemberPreferencesServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.memberpreferences;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MemberPreferencesServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MemberPreferencesServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/memberpreferences/MemberPreferencesServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.memberpreferences;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MemberPreferencesServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
